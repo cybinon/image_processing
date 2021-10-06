31146224b01c96aef1ba0ec7e0d5cc4e1c51f079
nikorunikk <nikorunikk@gitlab.com>
2021-10-06 15:36:16 +0800

    lab3 done

--- lab2/lab2-3.py
+++ lab2/lab2-3.py
@@ -1,27 +1,48 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
-img = cv2.imread('Fig0310.tif')
-img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+img = cv2.imread('lab2/Fig0310.tif')
+img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY);
 
+
+
+fig = plt.figure();
 x = np.arange(256)
 xp = [0, 64, 192, 255]
 fp = [0, 16, 240, 255]
 table = np.interp(x, xp, fp).astype('uint8')
 img1 = cv2.LUT(img, table)
-cv2.imshow("Output1", img1)
+
+
+
+
+pic_subplot = plt.subplot(3,2,1)
+pic_subplot.imshow(img1)
+pic_subplot = plt.subplot(3,2,2)
+pic_subplot.plot(xp,fp)
 
 xp = [0, 90, 192, 255]
 fp = [0, 32, 150, 255]
 table = np.interp(x, xp, fp).astype('uint8')
 img2 = cv2.LUT(img, table)
-cv2.imshow("Output2", img2)
+
+pic_subplot = plt.subplot(3,2,3)
+pic_subplot.imshow(img2)
+pic_subplot = plt.subplot(3,2,4)
+pic_subplot.plot(xp,fp)
 
 xp = [0, 115, 128, 100, 255]
 fp = [0,  0,  128, 255, 255]
 table = np.interp(x, xp, fp).astype('uint8')
 img3 = cv2.LUT(img, table)
-cv2.imshow("Output3", img3)
+
+pic_subplot = plt.subplot(3,2,5)
+pic_subplot.imshow(img3)
+pic_subplot = plt.subplot(3,2,6)
+pic_subplot.plot(xp,fp)
+
+plt.show()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows() 
